c3d47c9d6064563ccf14b45781b8f76661111342
Lynx-Lynx <delinne.weird@gmail.com>
2021-08-11 23:48:30 +0300

    fixed sending mails

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require('express');
 const app = express();
 const nodemailer = require("nodemailer");
@@ -5,77 +6,50 @@ const port = 3001;
 const router = express.Router();
 const cors = require('cors');
 require('dotenv').config();
+let bodyParser = require('body-parser');
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use('/', router);
-app.use(express.json());
 
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
-  }
-});
+let jsonParser = bodyParser.json();
 
-router.post('/send', (req, res, next) => {
-  let name = req.body.name
-  let email = req.body.email
-  let message = req.body.message
-  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+async function main() {
 
-  let mailOptions = {
-    from: 'nodemailer@webapp.com',
-    to: 'delinne.weird@gmail.com',
-    subject: 'New message from contact form',
-    text: content
-  }
-
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
     }
+  });
+
+  router.post('/send', jsonParser, async (req, res, next) => {
+
+    let mailOptions = {
+      from: 'nodemailer@webapp.com',
+      to: 'delinne.weird@gmail.com',
+      subject: 'New message from contact form',
+      text: `You've got a new message from ${req.body.name}: "${req.body.message}". Provided contact information: ${req.body.email}, ${req.body.phone}.` 
+    }
+    console.log(mailOptions)
+
+    await transporter.sendMail(mailOptions, (err, data) => err ? res.json({status: 'fail'}) : res.json({status: 'success'}) )
   })
-})
 
-// router.post('/send', (req, res, next) => {
-//   let name = req.body.name;
-//   let email = req.body.email;
-//   let message = req.body.message;
-//   let content = `name: ${name} \n email: ${email} \n message: ${message} `
+};
 
-//   let mailOptions = {
-//     from: 'nodemailer@webapp.com',
-//     to: 'delinne.weird@gmail.com',
-//     subject: 'New message from contact form',
-//     text: content
-//   });
-
-// transporter.sendMail(mailOptions, function(err, data) {
-//   if (err) {
-//     console.log("Error " + err);
-//   } else {
-//     console.log("Email sent successfully");
-//   }
-// });
-
+main().catch(console.error);
 
 app.listen(port, () => {
   console.log(`app is listening at http://localhost:${port}`)
 });
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0' //without this throw 'Error: self signed certificate in certificate chain'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0' //without this throws 'Error: self signed certificate in certificate chain'
 //With it ' Warning: Setting the NODE_TLS_REJECT_UNAUTHORIZED environment variable to '0' makes TLS 
 //connections and HTTPS requests insecure by disabling certificate verification.'
